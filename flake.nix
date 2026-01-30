af1f2da21023d7134f4875fa472b317c4486f3df
Matt Robenolt <matt@ydekproductions.com>
2026-01-30 10:57:25 -0800

    add nix devShell

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "ziglint dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    mattware = {
+      url = "github:mattrobenolt/nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      mattware,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ mattware.overlays.default ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            zig_0_15
+            zls_0_15
+            zlint
+            zigdoc
+          ];
+
+          shellHook = ''
+            unset ZIG_GLOBAL_CACHE_DIR
+          '';
+        };
+      }
+    );
+}
